6b528644918f773db6817cdac18788526ce3313e
hhenu <henri.rapeli@gmail.com>
2024-09-28 21:06:23 +0300

    Some kind of implementation of WavWriter

--- wavfile_io.py
+++ wavfile_io.py
@@ -2,13 +2,14 @@
 Some functionality to write and read .wav files
 """
 
+import io
 import numpy as np
 
 from typing import Any
 
 HEADER = b"RIFF"
-FT_HEADER = b"WAVE"
-FORMAT_CHUNK = b"fmt "
+FORMAT_HEADER = b"WAVE"
+FORMAT_CHUNK_HEADER = b"fmt "
 FORMAT_DATA_LEN = 16
 PCM_INT = 1
 IEEE_FLOAT = 3
@@ -43,7 +44,7 @@ class WavReader:
         :return:
         """
         ft_header = data[8:12]
-        if ft_header != FT_HEADER:
+        if ft_header != FORMAT_HEADER:
             raise RuntimeError(f"Invalid file type header {ft_header.decode()}")
 
     @staticmethod
@@ -53,7 +54,7 @@ class WavReader:
         :return:
         """
         form_chunk = data[12:16]
-        if form_chunk != FORMAT_CHUNK:
+        if form_chunk != FORMAT_CHUNK_HEADER:
             raise RuntimeError(f"Invalid format chunk {form_chunk.decode()}")
 
     @staticmethod
@@ -178,12 +179,14 @@ class WavReader:
             elif bits_per_sample == 16:
                 dtype = np.int16
             else:
+                # TODO: I guess at least 24 bit ints are also supported by wav
                 raise RuntimeError(f"Integer data with size > 16 bits ({bits_per_sample})")
         # The validity of the format is already checked in _read_audio_format()
         else:
             if bits_per_sample == 32:
                 dtype = np.float32
             else:
+                # TODO: Perhaps other sizes should be allowed too
                 raise RuntimeError(f"Float format with size != 32 ({bits_per_sample})")
         
         buffer = np.zeros(shape=(samples, ), dtype=dtype)
@@ -201,15 +204,114 @@ class WavReader:
 
 
 class WavWriter:
-    def __init__(self, filepath: str) -> None:
+    def __init__(self, file_obj: io.BufferedWriter, channels: int, bits_per_sample: int,
+                 sample_rate: int) -> None:
         """
-        :param filepath:
+        :param file_obj: File handle to a opened file
+        :param channels: Amount of audio channels
+        :param bits_per_sample: AKA sample width
+        :param sample_rate: Sampling rate in Hz
         :return:
         """
-        self.filepath = filepath
-        self._size = 0
+        self.file_obj = file_obj
+        self.channels = channels
+        self.bits_per_sample = bits_per_sample
+        self.fs = sample_rate
+        self._total_size = 0
+        self._data_size = 0
+        self._write_num_channels()
+        self._write_sample_rate()
+        self._write_bits_per_sample()
+        self._write_bytes_per_sec()
+        self._write_bytes_per_block()
+        self._init_header()
+        self._total_size += 44
 
-    def write(self, indata: np.ndarray, frames: int, time: int, status: Any) -> None:
+    def _init_header(self) -> None:
+        """
+        Writes parts of the wav header but "dynamic" things like the file size will
+        need to be written/updated later
+        :return:
+        """
+        # Assure that we are at the start of the file
+        if self.file_obj.tell() != 0:
+            self.file_obj.seek(0)
+        self.file_obj.write(HEADER)
+        self.file_obj.seek(8)
+        self.file_obj.write(FORMAT_HEADER)
+        self.file_obj.write(FORMAT_CHUNK_HEADER)
+        self.file_obj.write(FORMAT_DATA_LEN.to_bytes(4, "little"))
+        self.file_obj.seek(36)
+        self.file_obj.write(DATA_HEADER)
+
+    def _write_file_size(self) -> None:
+        """
+        :return:
+        """
+        self.file_obj.seek(4)
+        self.file_obj.write(self._total_size.to_bytes(4, "little"))
+
+    def _write_audio_format(self, form: int) -> None:
+        """
+        :param form:
+        :return:
+        """
+        if form not in [PCM_INT, IEEE_FLOAT]:
+            raise RuntimeError(f"Unknown audio format {form}")
+        self.file_obj.seek(20)
+        self.file_obj.write(form.to_bytes(2, "little"))
+
+    def _write_num_channels(self) -> None:
+        """
+        :return:
+        """
+        self.file_obj.seek(22)
+        self.file_obj.write(self.channels.to_bytes(2, "little"))
+
+    def _write_sample_rate(self) -> None:
+        """
+        :return:
+        """
+        self.file_obj.seek(24)
+        self.file_obj.write(self.fs.to_bytes(4, "little"))
+
+    def _write_bytes_per_sec(self) -> None:
+        """
+        :return:
+        """
+        a = (self.fs * self.bits_per_sample * self.channels) / 8
+        # I suppose the decimal part should be 0
+        if not a.is_integer():
+            raise RuntimeError(f"Bytes per sec % 8 != 0: {a}")
+        self.file_obj.seek(28)
+        self.file_obj.write(int(a).to_bytes(4, "little"))
+
+    def _write_bytes_per_block(self) -> None:
+        """
+        :return:
+        """
+        a = (self.bits_per_sample * self.channels) / 8
+        # I suppose the decimal part should be 0
+        if not a.is_integer():
+            raise RuntimeError(f"Bytes per block % 8 != 0: {a}")
+        self.file_obj.seek(32)
+        self.file_obj.write(int(a).to_bytes(2, "little"))
+
+    def _write_bits_per_sample(self) -> None:
+        """
+        :return:
+        """
+        self.file_obj.seek(34)
+        self.file_obj.write(self.bits_per_sample.to_bytes(2, "little"))
+
+    def _write_data_size(self) -> None:
+        """
+        :return:
+        """
+        self.file_obj.seek(40)
+        self.file_obj.write(self._data_size.to_bytes(4, "little"))
+
+    def write_data(self, indata: np.ndarray, frames: int, time: int, status: Any) -> None:
         """
         :param indata:
         :param frames:
@@ -217,12 +319,34 @@ class WavWriter:
         :param status:
         :return:
         """
-        ...
+        # Mark these as unused for now
+        _ = frames
+        _ = time
+        _ = status
+        dtype = indata.dtype
+        # TODO: Perhaps we don't need to check the datatype every time this function
+        # is called
+        if dtype == np.int8:
+            self._write_audio_format(form=PCM_INT)
+        elif dtype == np.int16:
+            self._write_audio_format(form=PCM_INT)
+        elif dtype == np.float32:
+            self._write_audio_format(form=IEEE_FLOAT)
+        else:
+            raise RuntimeError(f"Unknown data type / audio format {dtype}")
+        new_bytes = indata.tobytes()
+        n = len(new_bytes)
+        self.file_obj.seek(0, io.SEEK_END)
+        self.file_obj.write(new_bytes)
+        self._data_size += n
+        self._total_size += n
+        self._write_file_size()
+        self._write_data_size()
 
     def get_written_bytes(self) -> int:
         """
         Returns the amount of bytes currently written to the file
         :return:
         """
-        return self._size
+        return self._total_size
 
